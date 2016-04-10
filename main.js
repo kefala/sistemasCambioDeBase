9823a7e77299255f10b05f2f7fab18c1e71ef579
Kefala <martin.g.lobo@gmail.com>
2016-04-10 19:43:08 -0300

    Ready

--- main.js
+++ main.js
@@ -1,92 +1,66 @@
-(function () {
-
-	var resultado = document.querySelector("#resultado"),
-	baseOfNumber = document.querySelector("#baseOfNumber"),
-	baseToTransform = document.querySelector("#baseToTransform"),
-	input = document.querySelector('input#campoUno');
-
 	document.addEventListener('DOMContentLoaded', init, false);
 
+	var a = document.querySelector("#baseOfNumber"),
+		b = document.querySelector("#baseToTransform");
+	
 	function init () {
-		input.addEventListener("keyup", calculate);
-
-		baseOfNumber.addEventListener("change", calculate);
-		
-		baseToTransform.addEventListener("change", calculate);
-	}
+		document.querySelector('input#campoUno').addEventListener("keyup", calculate);
+		a.addEventListener("change", calculate);
+		b.addEventListener("change", calculate);
+	};
 
 	function calculate () {
-		var number = parseFloat(input.value);
+		var number = document.querySelector('input#campoUno').value;
+		document.querySelector("#resultado").innerText = (a.value != 0) ? decimalTo(toDecimal(number, parseInt(a.value)), b.value) : decimalTo(number, b.value);
+	};
 
-		if (number === 0 || number) {
-			if (input.value.indexOf(",") === -1) {
-				resultado.innerHTML = "Cargando";
-				
-				if (baseOfNumber.value == 10) {
-					var resultadoReal = decimalTo(number, baseToTransform.value);
-					resultado.innerHTML = resultadoReal.toString();
-				} else {
-					if (baseToTransform.value == baseOfNumber.value) {
-						resultado.innerHTML = input.value;
-					} else if (baseToTransform.value != 10) {
-						resultado.innerHTML = convertThroughDecima();
-					} else {
-						if (input.value.indexOf(".") === -1) {
-							resultado.innerHTML = otherBaseToDecimal(input.value);
-						} else {
-							resultado.innerHTML = otherBaseToDecimal(input.value.replace('.', ''));
-						}
-					}
-				}
 
-			} else {
-				resultado.innerHTML = "Por favor, reemplace la coma por un punto";
-			}
+	function decimalTo (numero, base){
+		return parseFloat(numero).toString(parseInt(base)); 
+	};
 
-		} else {
-			resultado.innerHTML = "Ingrese un numero correcto";
-		};
-	}
 
-	function decimalTo (number, base) {
-		if (typeof(number) == "number") {
-			return (number).toString(base);
-		} else {
-			resultado.innerHTML = "Ocurrió un error inesperado";
-		};
-	}
+	function toDecimal (string, base) {
+		var aux,
+			newString = 0,
+			decimalCharacters = 0;
 
-	function otherBaseToDecimal (string) {
+		if (string.indexOf(".") !== -1)
+			decimalCharacters = string.length - string.indexOf(".") - 1;
+			
+		string = string.replace('.', '').split("").reverse().join("");	//saco el punto e invierto la cadena
 
-		var flag = 0,
-		plus,
-		newString = 0;
+		for (var i = 0; i < string.length; i++) {
+			aux = convertChartInNumer(string[i]) * window.Math.pow(parseFloat(base),(i-decimalCharacters));
 
-		for (var i = string.length; i > 0; i--) {
-			plus = (string[flag] * window.Math.pow(parseFloat(baseOfNumber.value),(i-1)));
-
-			if (i === string.length) {
-				newString = parseFloat(plus);
-			} else {
-				newString = parseFloat(newString) + parseFloat(plus);
-			}
-
-			flag++;				
-		}
+			newString = (i === string.length) ? parseFloat(aux) : parseFloat(newString) + parseFloat(aux);
+		};		
 
 		return newString;
-	}
+	};
 
-	function convertThroughDecima () {
-		var toDecimal;
-
-		if (input.value.indexOf(".") === -1) {
-			toDecimal = otherBaseToDecimal(input.value);
-		} else {
-			toDecimal = otherBaseToDecimal(input.value.replace('.', ''));
+	function convertChartInNumer (chart) {
+		switch(chart) {
+			case "a":
+				return 10;
+			break;
+			case "b":
+				return 11;
+			break;
+			case "c":
+				return 12;
+			break;
+			case "d":
+				return 13;
+			break;
+			case "e":
+				return 14;
+			break;
+			case "f":
+				return 15;
+			break;
+			default:
+				return chart;
+			break;
 		}
-		
-		return decimalTo(toDecimal, baseToTransform.value);
 	}
-
-})();
